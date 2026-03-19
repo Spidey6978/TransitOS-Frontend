e0476211fcb06c44d5128f2724cc0dde234b0336
Spidey6978 <veergopani55@gmail.com>
2026-03-19 19:23:43 +0530

    dashboard embed

--- src/pages/TrafficMap/trafficmap.jsx
+++ src/pages/TrafficMap/trafficmap.jsx
@@ -2,7 +2,7 @@ export default function TrafficMap() {
   return (
     <div className="w-full h-[calc(100vh-60px)] bg-slate-950">
       <iframe 
-        src="http://localhost:8501/?embed=true" 
+        src="https://transitos-aw5w.onrender.com/?embed=true" 
         className="w-full h-full border-none"
         title="TransitOS Traffic Mesh"
         allow="autoplay; fullscreen"
